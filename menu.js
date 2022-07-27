8224ba5dcec3485a575619eaf65f7fd4619d15fb
Jose Antonio González <joseantonioga96@gmail.com>
2022-07-27 20:54:30 +0200

    fixed background

--- menu.js
+++ menu.js
@@ -29,21 +29,9 @@ document.addEventListener("click", function (e) {
 });
 
 window.onload = function () {
-  // onmousemove es el evento que detecta cada movimiento
-  // del cursor sobre lo que abarca el cuerpo de la página
-  // el cual nos envía la variable 'e' que contiene clientX
-  // y clientY, las coordenadas del cursor
   document.onmousemove = function (e) {
-    // la posición se calcula de acuerdo a X, pero
-    // se divide entre 10, para que tenga un movimiento
-    // más suave con respecto al movimiento real
-    // del cursor del mouse. Después se hace negativo,
-    // para que este sea en dirección contrario, con una
-    // sensación de desplazamiento más que de arrastre.
     var x = -(e.clientX / 10);
-    // lo mismo para Y
     var y = -(e.clientY / 10);
-    // backgroundPosition son las coordenadas del fondo
     this.body.style.backgroundPosition = x + "px " + y + "px";
   };
 };
